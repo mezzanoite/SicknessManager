9019690e54d96acb4f0d48cd5b5a9b8c55a3a8f0
Rafael Mezzanotte Soares <rafael.mezzanotte@gmail.com>
2018-05-20 17:07:34 -0300

    Adicionando pickers de data e hora

--- app/src/main/java/br/com/mezzanotte/sicknessmanager/RegisterActivity.kt
+++ app/src/main/java/br/com/mezzanotte/sicknessmanager/RegisterActivity.kt
@@ -1,8 +1,19 @@
 package br.com.mezzanotte.sicknessmanager
 
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.text.format.DateFormat
 import android.view.MenuItem
+import android.widget.TextView
+import android.widget.TimePicker
+import kotlinx.android.synthetic.main.activity_register.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -10,6 +21,8 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        this.getTime(tvHour, this)
+        this.getDate(tvDate, this)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -18,6 +31,45 @@ class RegisterActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    fun getTime(textView: TextView, context: Context){
+
+        val cal = Calendar.getInstance()
+
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { _, hour, minute ->
+            cal.set(Calendar.HOUR_OF_DAY, hour)
+            cal.set(Calendar.MINUTE, minute)
+            textView.text = SimpleDateFormat("HH:mm").format(cal.time)
+        }
+
+        btHour.setOnClickListener {
+            TimePickerDialog(context, timeSetListener,
+                    cal.get(Calendar.HOUR_OF_DAY),
+                    cal.get(Calendar.MINUTE),
+                    true).show()
+        }
+    }
+
+    fun getDate(textView: TextView, context: Context){
+
+        val cal = Calendar.getInstance()
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, monthOfYear)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            textView.text = SimpleDateFormat("dd.MM.yyyy").format(cal.time)
+
+        }
+
+        btDate.setOnClickListener {
+            DatePickerDialog(context, dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
     }
 
 }
